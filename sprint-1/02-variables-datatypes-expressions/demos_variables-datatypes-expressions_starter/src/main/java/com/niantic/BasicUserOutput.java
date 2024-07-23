ab4a3e97f95cf7bc7fdb8d2ac8135fd12c36159d
Valerie Kuan <valeriekuan01@gmail.com>
2024-07-23 14:35:08 -0700

    learning variables in java

--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/BasicUserOutput.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/BasicUserOutput.java
@@ -4,6 +4,18 @@ public class BasicUserOutput
 {
     public static void main(String[] args)
     {
+        System.out.println("Hello World!");
 
+        int quantity = 150;
+        System.out.println(quantity);
+
+        int milesToPluto = Integer.MAX_VALUE;
+        System.out.println(milesToPluto);
+        milesToPluto = milesToPluto * 2;
+        System.out.println(milesToPluto);
+
+        long age = 5000;
+        int smallAge = (int)age;
+        System.out.println(age + " : " + smallAge);
     }
 }
--- sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
+++ sprint-1/02-variables-datatypes-expressions/demos_variables-datatypes-expressions_starter/src/main/java/com/niantic/Calculations.java
@@ -4,6 +4,22 @@ public class Calculations
 {
     public static void main(String[] args)
     {
+        // Calculate how many donuts each student can have
+        int numberOfStudents = 4;
+        int numberOfDonuts = 22;
+
+        int donutsPerStudent = numberOfDonuts / numberOfStudents;
+        int remainingDonuts = numberOfDonuts % numberOfStudents;
+        double donutsPerStudentDouble = (double)numberOfDonuts / numberOfStudents;
+
+        System.out.println("Number of donuts per student");
+        System.out.println("Total Donuts:       " + numberOfDonuts);
+        System.out.println("Total students:     " + numberOfStudents);
+        System.out.println("Donuts per student: " + donutsPerStudent);
+        System.out.println("Remaining donuts:   " + remainingDonuts);
+        System.out.println();
+
+        System.out.println("Donuts per student (double): " + donutsPerStudentDouble);
 
     }
 }
