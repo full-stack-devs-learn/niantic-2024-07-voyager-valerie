06c953042b772eed326f61c97aeba6cfe81af5d6
Valerie Kuan <valeriekuan01@gmail.com>
2024-07-24 13:13:20 -0700

    in class demo and notes

---
+++ sprint-1/03-functions-conditions/demos_functions-conditions_starter/src/main/java/com/niantic/REview.java
@@ -0,0 +1,15 @@
+package com.niantic;
+
+public class REview
+{
+    public static void main(String[] args)
+    {
+        float smallerNumber = Float.MAX_VALUE;  // wrapper class; lowercase version is the primitive data type
+        double biggerNumber = Double.MAX_VALUE;
+
+        System.out.println(smallerNumber);
+        System.out.println(biggerNumber);
+
+        double price = 12.5;
+    }
+}
