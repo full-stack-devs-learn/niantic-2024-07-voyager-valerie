9c89dcb6866e69b58ded9d00e975e7b737aa9629
Valerie Kuan <valeriekuan01@gmail.com>
2024-08-02 15:36:35 -0700

    separated database design and seed data from scripts

--- sprint-1/09-database-design/exercise_database-design/database_design.sql
+++ sprint-1/09-database-design/exercise_database-design/database_design.sql
@@ -1,30 +1,29 @@
 DROP DATABASE IF EXISTS transactions;
-
 CREATE DATABASE transactions;
-
 USE transactions;
 
 CREATE TABLE person
 (
-	person_id INT NOT NULL AUTO_INCREMENT,
-    person_name VARCHAR(20) NOT NULL,
+    person_id INT NOT NULL AUTO_INCREMENT,
+    person_name VARCHAR(20),
     PRIMARY KEY (person_id)
 );
 
 CREATE TABLE category
 (
-	category_id INT NOT NULL,
-    category_name VARCHAR(40) NOT NULL,
+    category_id INT NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(40),
     person_id INT NOT NULL,
     amount DECIMAL(10, 2),
     date DATETIME,
+    category_order INT,
     PRIMARY KEY (category_id),
-    FOREIGN KEY (person_id) REFERENCES person (person_id)
+    FOREIGN KEY (person_id) REFERENCES person(person_id)
 );
 
 CREATE TABLE sub_category
 (
-	sub_category_id INT NOT NULL AUTO_INCREMENT,
+    sub_category_id INT NOT NULL AUTO_INCREMENT,
     sub_category_name VARCHAR(40),
     category_id INT NOT NULL,
     notes VARCHAR(200),
@@ -34,38 +33,59 @@ CREATE TABLE sub_category
 
 CREATE TABLE vendor
 (
-	vendor_id INT NOT NULL AUTO_INCREMENT,
+    vendor_id INT NOT NULL AUTO_INCREMENT,
     vendor_name VARCHAR(40),
     person_id INT NOT NULL,
     PRIMARY KEY (vendor_id),
-    FOREIGN KEY (person_id) REFERENCES person (person_id)
+    FOREIGN KEY (person_id) REFERENCES person(person_id)
 );
 
+-- seed data
 INSERT INTO person(person_name)
 VALUES ('John'),
 	('Sarah');
 
-INSERT INTO category(category_id, category_name, person_id, amount, date)
-VALUES (1, 'Housing', 1, 1200, '2024-05-01'),
-	(2, 'Transportation', 2, 40, '2024-05-03'),
-    (3, 'Food', 1, 150, '2024-05-05'),
-    (4, 'Healthcare', 2, 20, '2024-05-07'),
-    (5, 'Entertainment', 1, 30, '2024-05-10'),
-    (6, 'Vacation/Travel', 2, 300, '2024-05-12'),
-    (1, 'Housing', 1, 100, '2024-05-15'),
-    (2, 'Transportation', 2, 20, '2024-05-17'),
-    (3, 'Food', 1, 25, '2024-05-20'),
-    (4, 'Healthcare', 2, 50, '2024-05-22'),
-    (5, 'Entertainment', 1, 15, '2024-05-24'),
-    (6, 'Vacation/Travel', 2, 200, '2024-05-28'),
-    (1, 'Housing', 2, 50, '2024-05-01'),
-    (2, 'Transportation', 1, 100, '2024-05-04'),
-    (3, 'Food', 2, 10, '2024-05-09'),
-    (4, 'Healthcare', 1, 15, '2024-05-14'),
-    (5, 'Entertainment', 2, 50, '2024-05-18'),
-    (6, 'Vacation/Travel', 1, 150, '2024-05-23'),
-    (3, 'Food', 2, 80, '2024-05-26'),
-    (1, 'Housing', 1, 60, '2024-05-30');
+
+INSERT INTO category(category_name, person_id, amount, date, category_order)
+VALUES ('Housing', 1, 1200, '2024-05-01', 1),
+	('Transportation', 2, 40, '2024-05-03', 2),
+    ('Food', 1, 150, '2024-05-05', 3),
+    ('Healthcare', 2, 20, '2024-05-07', 4),
+    ('Entertainment', 1, 30, '2024-05-10', 5),
+    ('Vacation/Travel', 2, 300, '2024-05-12', 6),
+    ('Housing', 1, 100, '2024-05-15', 1),
+    ('Transportation', 2, 20, '2024-05-17', 2),
+    ('Food', 1, 25, '2024-05-20', 3),
+    ('Healthcare', 2, 50, '2024-05-22', 4),
+    ('Entertainment', 1, 15, '2024-05-24', 5),
+    ('Vacation/Travel', 2, 200, '2024-05-28', 6),
+    ('Housing', 2, 50, '2024-05-01', 1),
+    ('Transportation', 1, 100, '2024-05-04', 2),
+    ('Food', 2, 10, '2024-05-09', 3),
+    ('Healthcare', 1, 15, '2024-05-14', 4),
+    ('Entertainment', 2, 50, '2024-05-18', 5),
+    ('Vacation/Travel', 1, 150, '2024-05-23', 6),
+    ('Food', 2, 80, '2024-05-26', 3),
+    ('Housing', 1, 60, '2024-05-30', 1),
+    ('Housing', 1, 1300, '2024-06-01', 1),
+    ('Transportation', 2, 60, '2024-06-03', 2),
+    ('Food', 1, 200, '2024-06-10', 3),
+    ('Healthcare', 2, 30, '2024-06-15', 4),
+    ('Entertainment', 1, 40, '2024-06-20', 5),
+    ('Vacation/Travel', 2, 350, '2024-06-25', 6),
+    ('Housing', 1, 1100, '2024-07-01', 1),
+    ('Transportation', 2, 50, '2024-07-05', 2),
+    ('Food', 1, 180, '2024-07-10', 3),
+    ('Healthcare', 2, 40, '2024-07-15', 4),
+    ('Entertainment', 1, 25, '2024-07-20', 5),
+    ('Vacation/Travel', 2, 300, '2024-07-25', 6),
+    ('Housing', 2, 60, '2024-08-01', 1),
+    ('Transportation', 1, 120, '2024-08-03', 2),
+    ('Food', 2, 90, '2024-08-10', 3),
+    ('Healthcare', 1, 25, '2024-08-15', 4),
+    ('Entertainment', 2, 55, '2024-08-20', 5),
+    ('Vacation/Travel', 1, 180, '2024-08-25', 6),
+    ('Food', 1, 70, '2024-08-30', 3);
 
 
 INSERT INTO sub_category(sub_category_name, category_id, notes)
@@ -75,16 +95,101 @@ VALUES ('Rent', 1, 'Monthly rent payment'),
     ('Rx', 4, 'Prescription refill'),
     ('Movies', 5, 'Movie night out'),
     ('Flight', 6, 'Booked flight for upcoming trip'),
-    ('Utilities', 1, 'Booked flight for upcoming trip'),
-    ('Subway', 2, 'Monthly subway pass'),
-    ('Restaurant', 3, 'Dinner with friends'),
-    ('Dr Visit', 4, 'Routine check-up'),
-    ('Netflix', 5, 'Monthly subscription fee'),
-    ('Hotel', 6, 'Hotel booking for vacation'),
-    ('Utilities', 1, 'Water bill payment'),
-    ('Car maintanence', 2, 'Car oil change'),
-    ('Snacks', 3, 'Snacks for movie night'),
-    ('OTC Medicine', 4, 'Cold medicine'),
-    ('Sports', 5, 'Tickets to a baseball game'),
-    ('Car rental', 6, 'Rental car for trip'),
-    ('Groceries', 3, 'Fresh produce and groceries');
+    ('Utilities', 7, 'Electricity bill payment'),
+    ('Subway', 8, 'Monthly subway pass'),
+    ('Restaurant', 9, 'Dinner with friends'),
+    ('Dr Visit', 10, 'Routine check-up'),
+    ('Netflix', 11, 'Monthly subscription fee'),
+    ('Hotel', 12, 'Hotel booking for vacation'),
+    ('Utilities', 13, 'Water bill payment'),
+    ('Car maintanence', 14, 'Car oil change'),
+    ('Snacks', 15, 'Snacks for movie night'),
+    ('OTC Medicine', 16, 'Cold medicine'),
+    ('Sports', 17, 'Tickets to a baseball game'),
+    ('Car rental', 18, 'Rental car for trip'),
+    ('Groceries', 19, 'Fresh produce and groceries'),
+    ('Utilities', 20, 'Internet service provider bill'),
+    ('Rent', 1, 'Monthly rent payment for June'),
+    ('Gas', 2, 'Fuel cost for June'),
+    ('Groceries', 3, 'Monthly grocery shopping for June'),
+    ('Rx', 4, 'Medical expenses for June'),
+    ('Movies', 5, 'Cinema tickets for June'),
+    ('Flight', 6, 'Flight booking for June vacation'),
+    ('Utilities', 1, 'Electricity bill for June'),
+    ('Subway', 2, 'Transportation pass for June'),
+    ('Restaurant', 3, 'Dining out in June'),
+    ('Dr Visit', 4, 'Check-up in June'),
+    ('Netflix', 5, 'Subscription fee for June'),
+    ('Hotel', 6, 'Hotel stay for June vacation'),
+    ('Utilities', 1, 'Water bill for June'),
+    ('Car maintenance', 2, 'Maintenance service for June'),
+    ('Snacks', 3, 'Snacks bought in June'),
+    ('OTC Medicine', 4, 'Over-the-counter medicine purchased in June'),
+    ('Sports', 5, 'Baseball game tickets for June'),
+    ('Car rental', 6, 'Car rental for June trip'),
+    ('Groceries', 3, 'Fresh produce bought in June'),
+    ('Utilities', 1, 'Internet bill for June'),
+    ('Rent', 1, 'Monthly rent payment for July'),
+    ('Gas', 2, 'Fuel cost for July'),
+    ('Groceries', 3, 'Monthly grocery shopping for July'),
+    ('Rx', 4, 'Medical expenses for July'),
+    ('Movies', 5, 'Cinema tickets for July'),
+    ('Flight', 6, 'Flight booking for July vacation'),
+    ('Utilities', 1, 'Electricity bill for July'),
+    ('Subway', 2, 'Transportation pass for July'),
+    ('Restaurant', 3, 'Dining out in July'),
+    ('Dr Visit', 4, 'Check-up in July'),
+    ('Netflix', 5, 'Subscription fee for July'),
+    ('Hotel', 6, 'Hotel stay for July vacation'),
+    ('Utilities', 1, 'Water bill for July'),
+    ('Car maintenance', 2, 'Maintenance service for July'),
+    ('Snacks', 3, 'Snacks bought in July'),
+    ('OTC Medicine', 4, 'Over-the-counter medicine purchased in July'),
+    ('Sports', 5, 'Baseball game tickets for July'),
+    ('Car rental', 6, 'Car rental for July trip'),
+    ('Groceries', 3, 'Fresh produce bought in July'),
+    ('Utilities', 1, 'Internet bill for July');
+
+    
+INSERT INTO vendor(vendor_name, person_id)
+VALUES('Landlord Inc.', 1),
+	('Shell Gas Station', 2),
+    ('Fresh Mart', 1),
+    ('CVS Pharmacy', 2),
+    ('Cinemax Theater', 1),
+    ('Delta Airlines', 2),
+    ('Powerco', 1),
+    ('Metro Transit', 2),
+    ('Joe''s Diner', 2),
+    ('HealthFirst Clinic', 2),
+    ('Netflix', 1),
+    ('Oceanfront Resort', 2),
+    ('WaterWorks', 2),
+    ('AutoWorks', 1),
+    ('Snack Haven', 2),
+    ('Pharmax', 1),
+    ('Sports Arena', 2),
+    ('Zoom Car Rentals', 1),
+    ('Farmer''s Market', 2),
+    ('FastNet', 1),
+    ('Grocery Store', 1),
+    ('Bookstore', 2),
+    ('Gym', 1),
+    ('Coffee Shop', 2),
+    ('Spa', 1),
+    ('Online Retailer', 2),
+    ('Dry Cleaners', 1),
+    ('Pharmacy', 2),
+    ('Furniture Store', 1),
+    ('Supermarket', 2),
+    ('Car Dealership', 1),
+    ('Pet Store', 2),
+    ('Bike Shop', 1),
+    ('Music Store', 2),
+    ('Home Improvement Store', 1),
+    ('Florist', 2),
+    ('Craft Store', 1),
+    ('Electronics Store', 2),
+    ('Lawn Care Service', 1),
+    ('Seasonal Decoration Store', 2);
+    
---
+++ sprint-1/09-database-design/exercise_database-design/script.sql
@@ -0,0 +1,136 @@
+
+	-- script to add additional expenses
+    SET @category_id = 40,
+		@category_name = 'Food',
+        @person_id = 2,
+        @amount = 7,
+        @date = '2024-06-01',
+        @category_order = 3;
+	
+     SET @sub_category_name = 'Snacks', 
+		 @category_id = 40, 
+ 		 @notes = 'Oolong milk tea';
+         
+	SET @vendor_name = 'Sunright Tea Studio', 
+		@person_id = 2;    -- contributing expense to Sarah, who's an exisitng person in the db
+    
+    INSERT INTO person(person_name)
+    VALUES
+    (
+        @person_name
+    );
+    
+    INSERT INTO category(category_name, person_id, amount, date, category_order)
+    VALUES
+    (
+		@category_name, 
+        @person_id, 
+        @amount, 
+        @date, 
+        @category_order
+    );
+    
+    INSERT INTO sub_category(sub_category_name, category_id, notes)
+    VALUES
+    (
+		@sub_category_name, 
+        @category_id, 
+        @notes
+    );
+    
+    INSERT INTO vendor(vendor_name, person_id)
+    VALUES
+    (
+		@vendor_name,
+        @person_id
+    );
+    
+	
+    
+    -- all the expense categories and the expense distribution in a given month
+    
+    -- May Breakdown
+    SET @month := 5;
+    
+    SELECT category_order,
+		category_name,
+		SUM(amount) AS category_total
+    FROM category
+    WHERE MONTH(date) = @month
+    GROUP BY category_order, category_name;
+    
+    -- June Breakdown
+    SET @month := 6;
+    
+    SELECT category_order,
+		category_name,
+		SUM(amount) AS category_total
+    FROM category
+    WHERE MONTH(date) = @month
+    GROUP BY category_order, category_name;
+    
+    -- July Breakdown
+    SET @month := 7;
+    
+    SELECT category_order,
+		category_name,
+		SUM(amount) AS category_total
+    FROM category
+    WHERE MONTH(date) = @month
+    GROUP BY category_order, category_name;
+    
+    
+    -- all expenses broken down in a category in a given month
+     
+    SET @category_used := 'Housing',
+		@month := 5;
+    
+    SELECT category.category_name, sub_category.sub_category_name, amount, date
+    FROM category
+		JOIN sub_category ON category.category_id = sub_category.category_id
+    WHERE category.category_name = @category_used AND MONTH(date) = @month;
+    
+    
+    -- all transactions in a month made by person of choice
+    
+    SET @purchaser := 'Sarah',
+		@month := 7;
+    
+    SELECT category_name, amount, date, person_name
+    FROM category
+		JOIN person ON category.person_id = person.person_id
+    WHERE person_name = @purchaser AND month(date) = @month;
+    
+    -- all transactions made by person of choice in a year
+    SET @purchaser := 'John',
+		@year = 2024;
+    
+    SELECT category_name, amount, date, person_name
+    FROM category
+		JOIN person ON category.person_id = person.person_id
+    WHERE person_name = @purchaser AND YEAR(date) = @year;
+    
+    
+    -- identifying the expensive categories within a month, where the user defines "expensive"
+    
+    SET @expensive := 500,
+		@month := 5;
+    
+    SELECT category_name, 
+		SUM(amount) AS category_total
+    FROM category
+    WHERE MONTH(date) = @month
+    GROUP BY category_name
+    HAVING SUM(amount) > @expensive;
+    
+    -- identify expensive categories within a calendar year
+	SET @year := 2024;
+    
+	SELECT category_name,
+		SUM(amount) AS category_total
+    FROM category
+    WHERE YEAR(date) = @year
+    GROUP BY category_name
+    HAVING SUM(amount) > @expensive;
+    
+    
