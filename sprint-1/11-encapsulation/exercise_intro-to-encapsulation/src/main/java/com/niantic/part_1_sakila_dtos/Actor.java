21a704445905f7941942ddf0c8a0df016da1ae4c
Valerie Kuan <valeriekuan01@gmail.com>
2024-08-05 14:34:24 -0700

    completed DTO exercises

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -2,4 +2,54 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Actor
 {
+    private int id;
+    private String first_name;
+    private String last_name;
+
+    public Actor()
+    {
+
+    }
+
+    public Actor(int id, String first_name, String last_name)
+    {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public int getActorId()
+    {
+        return id;
+    }
+
+    public void setActorId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return first_name;
+    }
+
+    public void setFirstName(String first_name)
+    {
+        this.first_name = first_name;
+    }
+
+    public String getLastName()
+    {
+        return last_name;
+    }
+
+    public void setLastName(String last_name)
+    {
+        this.last_name = last_name;
+    }
+
+    public String getFullName()
+    {
+        return first_name + " " + last_name;
+    }
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Customer.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Customer.java
@@ -2,4 +2,138 @@ package com.niantic.part_1_sakila_dtos;
 
 public class Customer
 {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String zip;
+
+    public Customer()
+    {
+
+    }
+
+    public Customer(int id, String firstName, String lastName, String email, String phone, String address,
+                    String city, String state, String country, String zip)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zip = zip;
+    }
+
+    public int getCustomerId()
+    {
+        return id;
+    }
+
+    public void setCustomerId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getZip()
+    {
+        return zip;
+    }
+
+    public void setZip(String zip)
+    {
+        this.zip = zip;
+    }
+
+    public String getFullName()
+    {
+        return firstName + " " + lastName;
+    }
 }
