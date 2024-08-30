b4f289ded3245b3d30300a36bec296358c6d83a6
Gregor Dzierzon <gdzierzon@gmail.com>
2024-08-30 06:20:21 -0600

    Add demo starter code for javascript day 2

---
+++ sprint-2/10-DOM/demos_starter/it-tasks/js/app.js
@@ -0,0 +1,33 @@
+
+
+function displayTitle() {
+   
+}
+
+function displayTasks() {
+    
+}
+
+function createTaskDiv(task){
+
+}
+
+function buildTaskDate(task, parent){
+
+}
+
+function buildTaskPriorityPill(task, parent){
+
+}
+
+function buildTaskPriority(task, parent){
+
+}
+
+function buildTaskTitle(task, parent){
+
+}
+
+
+displayTitle();
+displayTasks();
