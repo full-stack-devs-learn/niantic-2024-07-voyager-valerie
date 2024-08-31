6f49971a96e16ab36318b4dc1cd539b69de6b30c
Valerie Kuan <valeriekuan01@gmail.com>
2024-08-30 21:41:43 -0700

    completed DOM exercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -6,13 +6,67 @@
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+const service = new ShoppingService();
 
+function displayListTitle() {
+    const listTitle = document.getElementById("title");
+    listTitle.textContent = service.getListName();
+}
+    
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+    const groceries = service.getShoppingList()
+                            .sort((left, right) => left.isComplete - right.isComplete)
+                            .map(grocery => {
+                                return {
+                                    ...grocery,
+                                    priorityText: grocery.isComplete == false
+                                                        ? "pending"
+                                                        : "complete"
+                                }
+                            })
 
+    const groceryListContainer = document.getElementById("shopping-list");
+
+    groceries.forEach(grocery => {
+        createShoppingDiv(grocery, groceryListContainer);
+    })
+}
+
+function createShoppingDiv(grocery, parent){
+    const shoppingDiv = document.createElement("div");
+
+    shoppingDiv.classList.add("list-item");
+
+    if(grocery.isComplete)
+    {
+        shoppingDiv.classList.add("complete");
+    }
+
+    parent.appendChild(shoppingDiv);
+
+    buildItemName(grocery, shoppingDiv);    
+    buildItemQuantity(grocery, shoppingDiv);
+}
+
+
+function buildItemName(grocery, parent){
+    const nameDiv = document.createElement("div");
+    nameDiv.classList.add("title");
+    nameDiv.textContent = grocery.title;
+
+    parent.appendChild(nameDiv);
+}
+
+function buildItemQuantity(grocery, parent){
+    const quantityDiv = document.createElement("div");
+    quantityDiv.classList.add("quantity");
+    quantityDiv.textContent = grocery.quantity;
+
+    parent.appendChild(quantityDiv);
+}
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
@@ -20,7 +74,11 @@ function displayGroceries() {}
 function markCompleted() {
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+    
+    displayListTitle();
+    displayGroceries();
+});
 
-displayListTitle();
-displayGroceries();
+
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,6 +1,7 @@
 class ShoppingService {
 
     getListName() {
+        return 'SHOPPING LIST';
     }
 
     getShoppingList() {
@@ -16,6 +17,54 @@ class ShoppingService {
                 title: 'Frozen Pizza',
                 quantity: 4,
                 isComplete: true
+            },
+            {
+                id: 3,
+                title: 'Hash Brown',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: 'Frozen Vegetables',
+                quantity: 1,
+                isComplete: true
+            },
+            {
+                id: 5,
+                title: 'Xiao Long Bao',
+                quantity: 3,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: 'Ice Cream',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: 'Pot Sticker',
+                quantity: 2,
+                isComplete: true
+            },
+            {
+                id: 8,
+                title: 'Popsicle',
+                quantity: 3,
+                isComplete: true
+            },
+            {
+                id: 9,
+                title: 'Shu Mai',
+                quantity: 5,
+                isComplete: true
+            },
+            {
+                id: 10,
+                title: 'Wonton',
+                quantity: 15,
+                isComplete: false
             }
         ];
     }
