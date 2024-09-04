da967bf57ac20714cade88fcab409e3d122fa884
Gregor Dzierzon <gdzierzon@gmail.com>
2024-09-04 08:55:19 -0600

    added demos

---
+++ sprint-2/12_javascript-and-thymeleaf/demos_sakila-website_starter/src/test/java/com/niantic/DemoSakilaWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niantic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSakilaWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
